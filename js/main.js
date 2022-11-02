6ef258c825511c3c7eab971b1124bd3d9220549e
Sofia-Citrini <sofiacitrini@gmail.com>
2022-11-02 18:43:34 +0100

    add word in boxes

--- js/main.js
+++ js/main.js
@@ -2,26 +2,22 @@ const boxesContainerEl = document.querySelector(".boxes_container");
 
 
 for (let i=1; i <= 100; i++) {
-    
-
-
     let color="";
 
-    // multiplo di 3 o multiplo di 5 e  multiplo sia di 3 che di 5
     if (i % 3 === 0 && i % 5 === 0){
         console.log ("FizzBuzz");
         color = "bg-red"; 
+        boxesContainerEl.innerHTML += `<div class="box ${color}">FizzBuzz</div>`;
     } else if (i % 5 === 0) {
         console.log ("Buzz");
         color = "bg-yellow";
+        boxesContainerEl.innerHTML += `<div class="box ${color}">Buzz</div>`;
     } else if(i % 3 === 0) {
         console.log("Fizz");
         color = "bg-green";
+        boxesContainerEl.innerHTML += `<div class="box ${color}">Fizz</div>`;
     } else {
         console.log(i);
+        boxesContainerEl.innerHTML += `<div class="box">${i}</div>`;
     }
-
-    boxesContainerEl.innerHTML += `<div class="box ${color}">${i}</div>`;
-
-    
 }
