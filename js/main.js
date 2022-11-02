f012f2268e4670b36558a47c79d50dee096d0456
Sofia-Citrini <sofiacitrini@gmail.com>
2022-11-02 18:31:42 +0100

    fixed console.log

--- js/main.js
+++ js/main.js
@@ -1,25 +1,27 @@
 const boxesContainerEl = document.querySelector(".boxes_container");
 
+
 for (let i=1; i <= 100; i++) {
     
-    console.log(`${i}`);
+
 
     let color="";
 
-    // multiplo di 3
-    if (i % 3 === 0) {
-        color = "bg-green";
-    }
-    
-    // multiplo di 5
-    if (i % 5 === 0) {
-        color = "bg-yellow";
-    }
-    
-    // multiplo sia di 3 che di 5
+    // multiplo di 3 o multiplo di 5 e  multiplo sia di 3 che di 5
     if (i % 3 === 0 && i % 5 === 0){
-        color = "bg-red";
+        console.log ("FizzBuzz");
+        color = "bg-red"; 
+    } else if (i % 5 === 0) {
+        console.log ("Buzz");
+        color = "bg-yellow";
+    } else if(i % 3 === 0) {
+        console.log("Fizz");
+        color = "bg-green";
+    } else {
+        console.log(i);
     }
 
-    boxesContainerEl.innerHTML += `<div class="box ${color}">${i}</div>`
+    boxesContainerEl.innerHTML += `<div class="box ${color}">${i}</div>`;
+
+    
 }
